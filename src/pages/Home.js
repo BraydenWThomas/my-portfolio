12949edac887a70520b0cbe0a7c3f2896e516169
BraydenWThomas <braybraybray5@gmail.com>
2024-11-14 01:51:46 +1100

    Testing Deploy Speed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
-      <p>This is where you'll get to know more about me and my work!</p>
+      <p>Get to know more about me and my work!</p>
     </div>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,7 @@ function Projects() {
   return (
     <div>
       <h1>My Projects</h1>
-      <p>A showcase of my work, with descriptions and links to demos and code.</p>
+      <p>A showcase of my previous projects</p>
     </div>
   );
 }
