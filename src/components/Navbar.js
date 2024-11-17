6323f5e4fa53928227eb4ca0ad53626afb8f6a6f
BraydenWThomas <braybraybray5@gmail.com>
2024-11-18 09:24:38 +1100

    Fix Navbar Collapsing Properly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import "./Navbar.css"
+import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  console.log(menuOpen);
+
   return (
     <nav>
       <div className='title'>Brayden</div>
-      <ul>
+      <div className="menu" onClick={(event) => { event.stopPropagation(); setMenuOpen(!menuOpen); }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/projects">Projects</NavLink></li>
