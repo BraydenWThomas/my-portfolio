7900fceeb64620bac711e31fcb844a6a3d759337
BraydenWThomas <braybraybray5@gmail.com>
2024-11-18 20:53:58 +1100

    Push Before Test With Material UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Typography } from '@mui/material';
+import "../styles/Home.css";
 
 function Home() {
+
   return (
     <div>
+
+      <Typography  >
+        Software Engineer
+      </Typography>
+      <Typography  
+      variant='h1'>
+        Hello I'm
+      </Typography>
+      <Typography  >
+        Brayden Thomas
+      </Typography>
+      <Typography  >
+        Small summary about me and my work
+      </Typography>
+
+      {/* <div className='body'>Test</div>
+      <div className='image'>Test</div>
+      <div className='links'>Test</div>
+      <div className='overview'>Test</div> */}
+
+
       <h1>Welcome to My Portfolio</h1>
       <p>Get to know more about me and my work!</p>
     </div>
