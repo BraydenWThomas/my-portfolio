31679bad8b3866b0619e9044926ab3a50fcfd083
BraydenWThomas <braybraybray5@gmail.com>
2024-12-14 22:08:25 +1100

    Projects Picture Expand Feature

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,21 @@
-import React from "react";
-import {Card,CardContent,CardMedia,Typography,Box,Chip,Button,} from "@mui/material";
+import React, { useState } from "react";
+import { Card, CardContent, CardMedia, Typography, Box, Chip, Button, Dialog, DialogContent } from "@mui/material";
 import { motion } from "framer-motion";
 import { useTheme, alpha } from "@mui/material/styles";
 
 const ProjectCard = ({ project }) => {
-  const theme = useTheme(); 
+  const theme = useTheme();
+  const [openOverlay, setOpenOverlay] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = () => {
+    setSelectedImage(project.image); // Set the selected image to the clicked image
+    setOpenOverlay(true); // Open the overlay
+  };
+
+  const handleCloseOverlay = () => {
+    setOpenOverlay(false); // Close the overlay
+  };
 
   return (
     <motion.div
@@ -17,12 +28,12 @@ const ProjectCard = ({ project }) => {
         sx={{
           maxWidth: 345,
           minWidth: 345,
-          minHeight:  470,
+          minHeight: 470,
           transition: "transform 0.3s",
           backgroundColor: alpha(theme.palette.background.default, 8),
-          boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)", 
+          boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
           "&:hover": {
-            backgroundColor: alpha(theme.palette.background.default, 9), 
+            backgroundColor: alpha(theme.palette.background.default, 9),
           },
         }}
       >
@@ -31,6 +42,8 @@ const ProjectCard = ({ project }) => {
           height="180"
           image={project.image}
           alt={`${project.name} thumbnail`}
+          onClick={handleImageClick} // Handle the click event to open the overlay
+          sx={{ cursor: "pointer" }}
         />
         <CardContent>
           <Typography variant="h5" component="div" gutterBottom color="#D8DBE2">
@@ -54,6 +67,19 @@ const ProjectCard = ({ project }) => {
           </Button>
         </CardContent>
       </Card>
+
+      {/* Full-Size Image Overlay */}
+      <Dialog open={openOverlay} onClose={handleCloseOverlay} maxWidth="lg" fullWidth>
+        <DialogContent>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <img
+              src={selectedImage}
+              alt="Full-size project"
+              style={{ width: '100%', height: 'auto' }}
+            />
+          </Box>
+        </DialogContent>
+      </Dialog>
     </motion.div>
   );
 };
